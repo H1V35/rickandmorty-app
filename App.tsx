25b26785dd0932d36e754d17f1239e361efa9fbf
H1V35 <higesjorge@gmail.com>
2024-04-05 15:39:49 +0200

    🎨 style: Change StatusBar barStyle to light-content

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { StatusBar } from 'react-native';
 
 import { queryClient } from '~/config/lib/tanstack-query';
 import { StackNavigator } from '~/presentation/navigation/StackNavigator';
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
+        <StatusBar barStyle="light-content" />
         <StackNavigator />
       </NavigationContainer>
     </QueryClientProvider>
