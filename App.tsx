1eb988429350a665c16908d12b023277e8f8b545
H1V35 <higesjorge@gmail.com>
2024-04-06 12:00:19 +0200

    🐛 fix: Fix status bar style bug

--- App.tsx
+++ App.tsx
@@ -4,13 +4,14 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from 'react-native';
 
 import { queryClient } from '~/config/lib/tanstack-query';
+import { appColors } from '~/config/theme/app-theme';
 import { StackNavigator } from '~/presentation/navigation/StackNavigator';
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <StatusBar barStyle="light-content" />
+        <StatusBar barStyle="light-content" backgroundColor={appColors.primary} />
         <StackNavigator />
       </NavigationContainer>
     </QueryClientProvider>
